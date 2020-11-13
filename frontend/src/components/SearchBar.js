e75b1bdb61f32a7ea4bd169529795251476f8400
MinhQuan Tran <minhquan.tran@wsu.edu>
2020-11-13 11:35:22 -0800

    Added AppBar with search bar

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,22 +1,69 @@
 import SearchBar from "material-ui-search-bar";
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CameraIcon from "@material-ui/icons/PhotoCamera";
 
-export default class TextBar extends React.Component{
-  constructor(props){
-    super(props)
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+export default class TextBar extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      value: ""
-    }
+      value: "",
+    };
   }
 
   render() {
-    return(
-      <SearchBar
-        value = {this.state.value}
-        onChange={(newValue) => this.setState({ value: newValue })}
-        onRequestSearch={() => alert(this.state.value)}
-        onClear={() => console.log('onClear')}
-      />
-    )
+    return (
+      <React.Fragment>
+        <AppBar position="relative">
+          <Toolbar>
+            <SearchBar
+              value={this.state.value}
+              onChange={(newValue) => this.setState({ value: newValue })}
+              onRequestSearch={() => alert(this.state.value)}
+              onClear={() => console.log("onClear")}
+              style={{
+                margin: "0 auto",
+                width: "80%"
+              }}
+            />
+          </Toolbar>
+        </AppBar>
+      </React.Fragment>
+    );
   }
 }
