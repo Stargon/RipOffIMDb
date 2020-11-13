cd6b144a69a81f4be14a243a8589b60b2093ae43
MinhQuan Tran <minhquan.tran@wsu.edu>
2020-11-13 15:26:58 -0800

    Removed unused onClear

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -6,9 +6,14 @@ import Toolbar from "@material-ui/core/Toolbar"
 export default class TextBar extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      value: "",
-    }
+    this.state = {}
+    // ES6 methods need to be binding to this instance
+    this.onRequestSearchEvent = this.onRequestSearchEvent.bind(this)
+  }
+
+  // Event handler to return the query
+  onRequestSearchEvent(query){
+    alert(query)
   }
 
   render() {
@@ -18,9 +23,8 @@ export default class TextBar extends React.Component {
           <Toolbar>
             <SearchBar
               value={this.state.value}
-              onChange={(newValue) => this.setState({ value: newValue })}
-              onRequestSearch={() => alert(this.state.value)}
-              onClear={() => console.log("onClear")}
+              onChange={(newValue) => this.setState({value: newValue})}
+              onRequestSearch={() => {this.onRequestSearchEvent(this.state.value)}}
               style={{
                 margin: "0 auto",
                 width: "80%"
