ef67fe867fac948ec52bcdf5a889f802f0bf23f4
MinhQuan Tran <minhquan.tran@wsu.edu>
2020-11-17 05:14:39 -0800

    Remove default background and text for the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,6 @@ function App() {
   return (
     <div className="App">
     <div className="App"><SearchEngine/></div>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
