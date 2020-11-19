ceea009d26932364300d2534bf25711e5f0f1059
Chelsea Sutcliffe <chelsea.dalgord@wsu.edu>
2020-11-19 09:47:57 -0800

    Update movie_database_whoosh.py
    connection of austin basic search and advanced search. Also, added section to open index if already created
    
    Co-Authored-By: Flashlock <65870500+Flashlock@users.noreply.github.com>

--- index/movie_database_whoosh.py
+++ index/movie_database_whoosh.py
@@ -14,9 +14,25 @@ csv_file = '../pre-processing/imdb_movie_database.csv'
 class WhooshSearch(object):
     def __init__(self):
         super(WhooshSearch, self).__init__()
+        self.page_size = 10
 
-    def basicSearch(self):
-        return
+    def basicSearch(self, query_entered):
+        returnables = list()
+        with self.indexer.searcher() as search:
+            query = MultifieldParser(['Title', 'Actors'], schema=self.indexer.schema)
+            query = query.parse(query_entered)
+            results = search.search(query, limit=None, terms=True)
+
+            # return the stored attributes
+            count = 0
+            for result in results:
+                if count == self.page_size:
+                    break
+                returnables.append((result['id'], result['page_url'], result['image_url'], result['Title'],
+                                    result['Actors'], result['Production'], result['Director'], result['Release_date'],
+                                    result['Genre'], result['Awards'], result['Critic_Score'], result['RunTime']))
+                count += 1
+            return returnables, len(results)
 
     def advancedSearch(self):
         ''' provides filters to search across multiple fields based on user input
@@ -44,7 +60,6 @@ class WhooshSearch(object):
         genres = list()
         release_date = list()
         runtime = list()
-
 
         # reset filter before each search
         allow_q = None
@@ -98,6 +113,7 @@ class WhooshSearch(object):
                 Only indexes if a database does not already exist
                 All fields of extracted data are indexed for the advanced search
                 """
+        # set up to only index once
         schema = Schema(id=ID(stored=True),
                                         image_url=TEXT(stored=True),
                                         page_url=TEXT(stored=True),
@@ -110,29 +126,34 @@ class WhooshSearch(object):
                                         Awards=TEXT(stored=True),
                                         Critic_Score=TEXT(stored=True),
                                         RunTime=TEXT(stored=True))
+
         if not os.path.exists('indexdir'):
             os.mkdir('indexdir')
 
-        indexer = create_in('indexdir', schema)
-        writer = indexer.writer()
+        if(exists_in('indexdir') != True):
+            indexer = create_in('indexdir', schema)
+            writer = indexer.writer()
 
-        df = pd.read_csv(csv_file)
+            df = pd.read_csv(csv_file)
 
-        for i in range(len(df)):
-            writer.add_document(id=str(df.loc[i, 'id']),
-                                                    image_url=str(df.loc[i, 'image_url']),
-                                                    page_url=str(df.loc[i, 'page_url']),
-                                                    Title=str(df.loc[i, 'Title']),
-                                                    Actors=str(df.loc[i, 'Actors']),
-                                                    Production=str(df.loc[i, 'Production']),
-                                                    Director=str(df.loc[i, 'Director']),
-                                                    Release_date=str(df.loc[i, 'Release_date']),
-                                                    Genre=str(df.loc[i, 'Genre']),
-                                                    Awards=str(df.loc[i, 'Awards']),
-                                                    Critic_Score=str(df.loc[i, 'Critic_Score']),
-                                                    RunTime=str(df.loc[i, 'Runtime']))
-        writer.commit()
-        self.indexer = indexer
+            for i in range(len(df)):
+                writer.add_document(id=str(df.loc[i, 'id']),
+                                                        image_url=str(df.loc[i, 'image_url']),
+                                                        page_url=str(df.loc[i, 'page_url']),
+                                                        Title=str(df.loc[i, 'Title']),
+                                                        Actors=str(df.loc[i, 'Actors']),
+                                                        Production=str(df.loc[i, 'Production']),
+                                                        Director=str(df.loc[i, 'Director']),
+                                                        Release_date=str(df.loc[i, 'Release_date']),
+                                                        Genre=str(df.loc[i, 'Genre']),
+                                                        Awards=str(df.loc[i, 'Awards']),
+                                                        Critic_Score=str(df.loc[i, 'Critic_Score']),
+                                                        RunTime=str(df.loc[i, 'Runtime']))
+            writer.commit()
+            self.indexer = indexer
+            
+        else:
+            self.indexer = open_dir('indexdir')
 
 if __name__ == '__main__':
     global theWhooshSearch
@@ -146,5 +167,8 @@ if __name__ == '__main__':
         elif searchType == 'a':
             theWhooshSearch.advancedSearch()
         else:
-            # change to match austins setup
-            theWhooshSearch.basicSearch()
+            query = input('Input a Query: ')
+            results, size = theWhooshSearch.basicSearch(query)
+            print('Number of results found: ' + str(size))
+            for result in results:
+                print(result[3])
