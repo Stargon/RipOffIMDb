6bdbe90bbb75418f36fe4b877894ea7dd96420a5
MinhQuan Tran <minhquan.tran@wsu.edu>
2020-11-28 13:54:36 -0800

    Added (roughly) more ui options to the search bar Preparing for advanced search features

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,21 +1,34 @@
-import SearchBar from "material-ui-search-bar"
-import React from "react"
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
+import SearchBar from "material-ui-search-bar";
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
 
 export default class TextBar extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = { ratingMin: 20, ratingMax: 37, ratingValue: 30 };
     // ES6 methods need to be binding to this instance
-    this.onRequestSearchEvent = this.onRequestSearchEvent.bind(this)
+    this.onRequestSearchEvent = this.onRequestSearchEvent.bind(this);
+    this.valuetext = this.valuetext.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   // Event handler to return the query
   onRequestSearchEvent(query) {
-    alert(query)
-    this.props.query(query)
+    alert(query);
+    this.props.query(query);
   }
+
+  valuetext(value) {
+    return `${value}°C`;
+  }
+  handleChange = (event, newValue) => {
+    this.setState({ ratingMax: newValue });
+  };
 
   render() {
     return (
@@ -25,13 +38,48 @@ export default class TextBar extends React.Component {
             <SearchBar
               value={this.state.value}
               onChange={(newValue) => this.setState({ value: newValue })}
-              onRequestSearch={() => { this.onRequestSearchEvent(this.state.value) }}
+              onRequestSearch={() => {
+                this.onRequestSearchEvent(this.state.value);
+              }}
               style={{
                 margin: "0 auto",
-                width: "80%"
+                width: "100%",
               }}
             />
           </Toolbar>
+          <div style={{ width: "100%" }}>
+            <Box display="flex" flexDirection="row" p={1} m={1}>
+              <Box p={1}>
+                <TextField
+                  defaultValue="actor"
+                  colorV="secondary"
+                  style={{ margine: "0 auto" }}
+                ></TextField>
+              </Box>
+              <Box p={1}>
+                <TextField
+                  defaultValue="actor"
+                  colorV="secondary"
+                  style={{ margine: "0 auto" }}
+                ></TextField>
+              </Box>
+              <Box p={1}>
+                <Typography id="range-slider" gutterBottom>
+                  Rating
+                </Typography>
+                <Slider
+                  value={this.state.rangeValue}
+                  onChange={(event, newValue) =>
+                    this.handleChange(event, newValue)
+                  }
+                  valueLabelDisplay="auto"
+                  aria-labelledby="range-slider"
+                  getAriaValueText={this.valuetext}
+                  color="secondary"
+                />
+              </Box>
+            </Box>
+          </div>
         </AppBar>
       </React.Fragment>
     );
