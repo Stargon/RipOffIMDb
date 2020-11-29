c87de75f6f241d024275596eddaf81c9056d71b1
MinhQuan Tran <minhquan.tran@wsu.edu>
2020-11-28 19:57:00 -0800

    Refactored to conditionally render components Will clean-up unused code later

--- frontend/src/components/SearchEngine.js
+++ frontend/src/components/SearchEngine.js
@@ -18,21 +18,24 @@ import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
 
 // Figure out fallback image later
-import FALLBACK_IMAGE from '../images/temp_fallback.png'
+import FALLBACK_IMAGE from "../images/temp_fallback.png";
 
 const example = [
   {
     id: 185004,
-    image: "https://m.media-amazon.com/images/M/MV5BMjA4ODMzNTE0OF5BMl5BanBnXkFtZTcwOTg5NDIzMQ@@._V1_SX300.jpg",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMjA4ODMzNTE0OF5BMl5BanBnXkFtZTcwOTg5NDIzMQ@@._V1_SX300.jpg",
     url: "https://www.imdb.com/title/tt0364977/",
     title: "Bangkok Haunted",
-    actors: "Pimsiree Pimsee, Pramote Seangsorn, Dawan Singha-Wee, Kalyanut Sriboonrueng",
+    actors:
+      "Pimsiree Pimsee, Pramote Seangsorn, Dawan Singha-Wee, Kalyanut Sriboonrueng",
     production: "N/A",
     director: "Oxide Chun Pang, Pisut Praesangeam",
     release_date: "26-Jul-05",
     genre: "Horror, Mystery",
     awards: "N/A",
-    critics: "[{'Source': 'Internet Movie Database', 'Value': '5.1/10'}, {'Source': 'Rotten Tomatoes', 'Value': '32%'}]",
+    critics:
+      "[{'Source': 'Internet Movie Database', 'Value': '5.1/10'}, {'Source': 'Rotten Tomatoes', 'Value': '32%'}]",
     runtime: "130 min",
   },
   {
@@ -73,6 +76,7 @@ export default class SearchEngine extends React.Component {
     this.makeStyles = this.makeStyles.bind(this);
     this.handleViewClick = this.handleViewClick.bind(this);
     this.handleImageError = this.handleImageError.bind(this);
+    this.renderSearch = this.renderSearch.bind(this);
   }
 
   handleQueryUpdate = (update) => {
@@ -83,9 +87,113 @@ export default class SearchEngine extends React.Component {
     window.open(url, "_blank");
   };
 
-  handleImageError = event => {
-      event.target.src = FALLBACK_IMAGE
-  }
+  handleImageError = (event) => {
+    event.target.src = FALLBACK_IMAGE;
+  };
+
+  renderSearch = (classes) => {
+    return (
+      <Grid item>
+        <CssBaseline />
+        {/* <AppBar position="relative">
+            <Toolbar>
+              <CameraIcon className={classes.icon} />
+              <Typography variant="h6" color="inherit" noWrap>
+                Album layout
+              </Typography>
+            </Toolbar>
+          </AppBar> */}
+        <main>
+          {/* Hero unit */}
+          {/* <div className={classes.heroContent}>
+              <Container maxWidth="sm">
+                <Typography
+                  component="h1"
+                  variant="h2"
+                  align="center"
+                  color="textPrimary"
+                  gutterBottom
+                >
+                  Album layout
+                </Typography>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  color="textSecondary"
+                  paragraph
+                >
+                  Something short and leading about the collection below—its
+                  contents, the creator, etc. Make it short and sweet, but not
+                  too short so folks don&apos;t simply skip over it entirely.
+                </Typography>
+                <div className={classes.heroButtons}>
+                  <Grid container spacing={2} justify="center">
+                    <Grid item>
+                      <Button variant="contained" color="primary">
+                        Main call to action
+                      </Button>
+                    </Grid>
+                    <Grid item>
+                      <Button variant="outlined" color="primary">
+                        Secondary action
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+              </Container>
+            </div> */}
+          <Container className={classes.cardGrid} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {
+                // From example constant, map a card for each element
+                // (adjust with json object later)
+                // Note that example uses labels that are capital, adjust them
+                // later based on response from Whoosh backend
+                example.map((movie) => (
+                  <Grid item key={movie} xs={12} sm={6} md={4}>
+                    <Card className={classes.card}>
+                      <CardMedia
+                        className={classes.cardMedia}
+                        component="img"
+                        image={movie.image}
+                        title={movie.title}
+                        onError={this.handleImageError}
+                      />
+                      <CardContent className={classes.cardContent}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {movie.title}
+                        </Typography>
+                        <Typography>
+                          {movie.genre.includes("N/A")
+                            ? "No genres listed"
+                            : movie.genre}
+                        </Typography>
+                        <Typography>
+                          {movie.runtime.includes("N/A")
+                            ? "Runtime unavailable"
+                            : movie.runtime}
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button
+                          size="small"
+                          onClick={() => this.handleViewClick(movie.url)}
+                          color="primary"
+                        >
+                          View
+                        </Button>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                ))
+              }
+            </Grid>
+          </Container>
+        </main>
+      </Grid>
+    );
+  };
 
   makeStyles(theme) {
     return {
@@ -123,115 +231,21 @@ export default class SearchEngine extends React.Component {
 
   render() {
     const update = this.state.query;
-    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const classes = makeStyles();
 
-    if (update !== "") {
-      //  render everything
-      return (
-        <React.Fragment>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                <TextBar query={this.handleQueryUpdate}></TextBar>
-                </Grid>
-                <Grid item>
-          <CssBaseline />
-          {/* <AppBar position="relative">
-            <Toolbar>
-              <CameraIcon className={classes.icon} />
-              <Typography variant="h6" color="inherit" noWrap>
-                Album layout
-              </Typography>
-            </Toolbar>
-          </AppBar> */}
-          <main>
-            {/* Hero unit */}
-            {/* <div className={classes.heroContent}>
-              <Container maxWidth="sm">
-                <Typography
-                  component="h1"
-                  variant="h2"
-                  align="center"
-                  color="textPrimary"
-                  gutterBottom
-                >
-                  Album layout
-                </Typography>
-                <Typography
-                  variant="h5"
-                  align="center"
-                  color="textSecondary"
-                  paragraph
-                >
-                  Something short and leading about the collection below—its
-                  contents, the creator, etc. Make it short and sweet, but not
-                  too short so folks don&apos;t simply skip over it entirely.
-                </Typography>
-                <div className={classes.heroButtons}>
-                  <Grid container spacing={2} justify="center">
-                    <Grid item>
-                      <Button variant="contained" color="primary">
-                        Main call to action
-                      </Button>
-                    </Grid>
-                    <Grid item>
-                      <Button variant="outlined" color="primary">
-                        Secondary action
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </div>
-              </Container>
-            </div> */}
-            <Container className={classes.cardGrid} maxWidth="md">
-              {/* End hero unit */}
-              <Grid container spacing={4}>
-                {
-                  // From example constant, map a card for each element
-                  // (adjust with json object later)
-                  // Note that example uses labels that are capital, adjust them
-                  // later based on response from Whoosh backend
-                  example.map((movie) => (
-                    <Grid item key={movie} xs={12} sm={6} md={4}>
-                      <Card className={classes.card}>
-                        <CardMedia
-                          className={classes.cardMedia}
-                          component="img"
-                          image={movie.image}
-                          title={movie.title}
-                          onError={this.handleImageError}
-                        />
-                        <CardContent className={classes.cardContent}>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            {movie.title}
-                          </Typography>
-                          <Typography>{movie.genre.includes("N/A")? "No genres listed" : movie.genre}</Typography>
-                          <Typography>{movie.runtime.includes("N/A")? "Runtime unavailable" : movie.runtime}</Typography>
-                        </CardContent>
-                        <CardActions>
-                          <Button
-                            size="small"
-                            onClick={() => this.handleViewClick(movie.url)}
-                            color="primary"
-                          >
-                            View
-                          </Button>
-                        </CardActions>
-                      </Card>
-                    </Grid>
-                  ))
-                }
-              </Grid>
-            </Container>
-          </main>
-
-                </Grid>
-            </Grid>
-        </React.Fragment>
-      );
-    } else {
-      // No results, just display searchbar
-      return <TextBar query={this.handleQueryUpdate}></TextBar>;
+    let results = null;
+    if (update !== "" && update !== undefined) {
+      results = this.renderSearch(classes);
     }
+    return (
+      <React.Fragment>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextBar query={this.handleQueryUpdate}></TextBar>
+          </Grid>
+          {results}
+        </Grid>
+      </React.Fragment>
+    );
   }
 }
