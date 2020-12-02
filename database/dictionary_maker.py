c88132d5ad2d33d55242441fa001e4c6f7461e4a
Flashlock <ajamesbetts1998@gmail.com>
2020-12-01 19:37:21 -0800

    Vocabulary has been generated

--- database/dictionary_maker.py
+++ database/dictionary_maker.py
@@ -1,33 +1,32 @@
+import pandas as pd
 import csv
 
 
 def main():
-    words=list()
-    with open('vocabulary.csv', 'w', newline='\n') as vocab:
-        writer=csv.writer(vocab)
-        with open('database_master.csv', 'r', newline='\n') as data:
-            reader = csv.reader(data)
-            next(reader)
-            for row in reader:
-                # title, actors, directors: 3, 4, 6
-                title = row[3]
-                actors=row[4]
-                directors=row[6]
-                title_words=title.split()
-                actor_words=actors.split()
-                director_words=directors.split()
-                for word in title_words:
-                    if not words.__contains__(word):
-                        words.append(word)
-                        writer.writerow([word])
-                for word in actor_words:
-                    if not words.__contains__(word):
-                        words.append(word)
-                        writer.writerow([word])
-                for word in director_words:
-                    if not words.__contains__(word):
-                        words.append(word)
-                        writer.writerow([word])
+    words = list()
+    with open('vocabulary.csv', 'w', encoding='utf-8', newline='\n') as vocab:
+        writer = csv.writer(vocab)
+        df = pd.read_csv('database_master.csv', engine='python')
+        for index, row in df.iterrows():
+            # title, actors, directors: 3, 4, 6
+            title = str(row[3]).lower()
+            actors = str(row[4]).lower()
+            directors = str(row[6]).lower()
+            title_words = title.split()
+            actor_words = actors.split(',')
+            director_words = directors.split(',')
+            for word in title_words:
+                if not words.__contains__(word):
+                    words.append(word)
+                    writer.writerow([word])
+            for word in actor_words:
+                if not words.__contains__(word):
+                    words.append(word)
+                    writer.writerow([word])
+            for word in director_words:
+                if not words.__contains__(word):
+                    words.append(word)
+                    writer.writerow([word])
 
 
 if __name__ == '__main__':
